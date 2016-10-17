eca218e4d0b0a129468656bd3b70b34c9b8aa74d
Dustin <dustin.kushnereit@mymail.champlain.edu>
2016-10-17 05:27:36 -0400

    Added in basis for the city. Buildings need single images that can be turned into a futuristic neon glow.

---
+++ Trunk/RobotleyCrue/Assets/Scripts/SkyBox/LightWall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightWall : MonoBehaviour
+{
+	Renderer renderer;
+    
+	void Start () 
+	{
+		renderer = GetComponent<Renderer>();
+	}
+	
+	void Update () 
+	{
+	
+		//if (Input.GetKeyDown(KeyCode.L))
+		{
+			Color final = Color.white * Mathf.LinearToGammaSpace(4);
+			renderer.material.SetColor("_EmissionColor", final);
+			DynamicGI.SetEmissive(renderer, final);
+		}
+
+    }
+}
